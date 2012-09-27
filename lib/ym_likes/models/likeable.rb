1edb46563610240f5a75913d92f9e49ea315f2e3
Matt Atkins <matt@yoomee.com>
2012-09-27 11:20:25 +0100

    Add liked_by scope to Likeable

--- lib/ym_likes/models/likeable.rb
+++ lib/ym_likes/models/likeable.rb
@@ -3,6 +3,7 @@ module YmLikes::Likeable
   def self.included(base)
     base.has_many :likes, :as => :resource, :class_name => 'YmLikes::Like', :dependent => :destroy
     base.scope :most_likes, base.joins("LEFT OUTER JOIN likes ON (likes.resource_id = #{base.table_name}.id AND likes.resource_type = '#{base.to_s}')").select("#{base.table_name}.*, COUNT(likes.id) AS like_count").order('like_count DESC').group("#{base.table_name}.id")
+    base.scope :liked_by, lambda{|user| base.joins(:likes).where(:likes => {:user_id => user.id})}
   end
   
 end
