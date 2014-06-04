65abef4247384f6948999c8716da8979b13df5fe
David Thompson <dtt101@gmail.com>
2014-06-04 17:30:18 +0100

    Fixed default scope for rails 4

--- lib/ym_likes/models/like.rb
+++ lib/ym_likes/models/like.rb
@@ -1,11 +1,14 @@
 module YmLikes::Like
-  
+
   def self.included(base)
     base.send(:include, YmCore::Model)
     base.belongs_to :user
-    base.belongs_to :resource, :polymorphic => true    
+    base.belongs_to :resource, :polymorphic => true
     base.validates_presence_of :user, :resource
-    base.send(:default_scope, base.where('likes.removed_at IS NULL'))
+    # base.send(:default_scope, base.where('likes.removed_at IS NULL'))
+    base.class_eval do
+      default_scope { where('likes.removed_at IS NULL') }
+    end
   end
-  
+
 end
