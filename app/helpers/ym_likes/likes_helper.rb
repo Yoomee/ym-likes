7497e8d18e9a6155624fe98455ad5bc007b383ae
David Thompson <dtt101@gmail.com>
2014-06-05 09:54:26 +0100

    Add check for current_user and updated css syntax

--- app/helpers/ym_likes/likes_helper.rb
+++ app/helpers/ym_likes/likes_helper.rb
@@ -5,7 +5,10 @@ module YmLikes::LikesHelper
     options[:class] = "#{options[:class]} like-link".strip
     resource_hash = {:resource_type => resource.class.to_s, :resource_id => resource.id}
     options[:data] = options.slice(:like_text, :unlike_text)
-    existing_like = ::Like.unscoped.where(resource_hash.merge(:user_id => current_user.id)).first
+    existing_like = nil
+    if current_user
+      existing_like = ::Like.unscoped.where(resource_hash.merge(:user_id => current_user.id)).first
+    end
     if existing_like.nil? || existing_like.removed_at.present?
       url = existing_like ? relike_like_path(existing_like) : create_like_path(resource_hash)
       options[:method] = existing_like ? :put : :post
@@ -14,14 +17,14 @@ module YmLikes::LikesHelper
       options[:title] = options.delete(:like_title)
     else
       url = unlike_like_path(existing_like)
-      options[:method] = :put      
+      options[:method] = :put
       options.delete(:like_text)
       link_text = options.delete(:unlike_text)
       options[:title] = options.delete(:unlike_title)
       options[:class] += " active"
     end
     link_to(url, options) do
-      content_tag(:i, "", :class => "icon-heart") + " #{link_text}"
+      content_tag(:i, "", :class => "heart") + " #{link_text}"
     end
 
   end
